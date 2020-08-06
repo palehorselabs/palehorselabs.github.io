34a49d06babe6b9cb6241bf4468150d4bac4c7d5
kwt <kwt@palehorselabs.com>
2020-08-06 06:02:12 -0500

    update text. update gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp');
-var csso = require('gulp-csso');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var cp = require('child_process');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const csso = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const cp = require('child_process');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync');
 
-var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
+const jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
 
 /*
  * Build the Jekyll Site
@@ -62,7 +62,7 @@ gulp.task('js', function(){
 });
 
 gulp.task('watch', function(done) {
-	gulp.watch('_sass/**/*.scss', gulp.series('sass'));
+	gulp.watch('_sass/**/*.scss', gulp.series('sass', 'jekyll-rebuild'));
 	gulp.watch('_js/**/*.js', gulp.series('js', 'jekyll-rebuild'));
 	gulp.watch('_assets/**/*.*', gulp.series('assets', 'jekyll-rebuild'));
 	gulp.watch(['*.html', '_includes/*.html', '_layouts/*.html', '_config.yml'], gulp.series('jekyll-rebuild'));
